1c7184ee9702b3287212fd5bfbddab74ff93d69a
shrutiPandey <shrutipandey23456@gmail.com>
2025-05-26 22:44:25 +0530

    JPQL Query

--- demo/src/main/java/com/jpa/demo/DemoApplication.java
+++ demo/src/main/java/com/jpa/demo/DemoApplication.java
@@ -1,5 +1,8 @@
 package com.jpa.demo;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,29 +13,40 @@ import com.jpa.demo.repo.*;
 @SpringBootApplication
 public class DemoApplication {
 
+	@Autowired
+    private final StudentRepo studentRepo;
+
+    DemoApplication(StudentRepo studentRepo) {
+        this.studentRepo = studentRepo;
+    }
+
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
 
 		StudentRepo repo = context.getBean(StudentRepo.class);
 
-		Student s1 = context.getBean(Student.class);
-		Student s2 = context.getBean(Student.class);
-		Student s3 = context.getBean(Student.class);
-
-		s1.setRollNo(1);
-		s1.setName("Anwesha");
-		s1.setMarks(100);
-
-		s2.setRollNo(2);
-		s2.setName("Ritu");
-		s2.setMarks(90);
-
-		s3.setRollNo(3);
-		s3.setName("Shruti");
-		s3.setMarks(80);
+//		Student s1 = context.getBean(Student.class);
+//		Student s2 = context.getBean(Student.class);
+//		Student s3 = context.getBean(Student.class);
+//
+//		s1.setRollNo(1);
+//		s1.setName("Anwesha");
+//		s1.setMarks(100);
+//
+//		s2.setRollNo(2);
+//		s2.setName("Ritu");
+//		s2.setMarks(90);
+//
+//		s3.setRollNo(3);
+//		s3.setName("Shruti");
+//		s3.setMarks(80);
 		
-		repo.save(s2);
-		repo.save(s3);
+//		System.out.println(repo.findAll());
+		Optional<Student> stud = repo.findById(2);
+		System.out.println(stud.orElse(new Student()));
+		
+		System.out.println(repo.findByName("Shruti"));
+		System.out.println(repo.findByMarksGreaterThan(80));
 	}
 
 }
--- demo/src/main/java/com/jpa/demo/repo/StudentRepo.java
+++ demo/src/main/java/com/jpa/demo/repo/StudentRepo.java
@@ -1,11 +1,18 @@
 package com.jpa.demo.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.jpa.demo.model.Student;
 
 @Repository
-public interface StudentRepo extends JpaRepository<Student, Integer>{
+public interface StudentRepo extends JpaRepository<Student, Integer> {
 
+    // @Query("Select s from Student s where s.name = ?1")
+    List<Student> findByName(String name);
+
+    List<Student> findByMarksGreaterThan(int marks);
 }
